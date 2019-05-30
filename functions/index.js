964b24f466bac89544798d7e870681cb194c0126
Roneet <roneetparida@gmail.com>
2019-05-30 09:01:29 -0400

    ui fixess

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,55 @@
 new Vue({
     el: '#wrapper',
     data: {
-        state: "Pause",
+        state: "Stopped",
         isPlay: false,
-
+        mic: 0,
+        recorder: 0,
+        soundfile: 0,
     },
     methods: {
         play: function() {
             this.isPlay = !this.isPlay;
-            this.state = "Pause";
-        },
+            this.state = "Recording";
+        }
     }
 })
+
+// navigator.mediaDevices.getUserMedia({
+//         audio: true
+//     })
+//     .then(stream => {
+//         handlerFunction(stream)
+//     })
+//
+//
+// function handlerFunction(stream) {
+//     rec = new MediaRecorder(stream);
+//     rec.ondataavailable = e => {
+//         audioChunks.push(e.data);
+//         if (rec.state == "inactive") {
+//             let blob = new Blob(audioChunks, {
+//                 type: 'audio/mpeg-3'
+//             });
+//             recordedAudio.src = URL.createObjectURL(blob);
+//             recordedAudio.controls = true;
+//             recordedAudio.autoplay = true;
+//             sendData(blob)
+//         }
+//     }
+// }
+//
+// function sendData(data) {}
+//
+// record.onclick = e => {
+//     record.disabled = true;
+//     stopRecord.disabled = false;
+//     audioChunks = [];
+//     rec.start();
+// }
+// stopRecord.onclick = e => {
+//     record.disabled = false;
+//     stop.disabled = true;
+//     rec.stop();
+//     document.querySelector(".content h2.state").textContent = "Stopped"
+// }
